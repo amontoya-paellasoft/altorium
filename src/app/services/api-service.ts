f75f18448f3f77e45823eacf14be42a3e80cbae5
Agus MT <amontoya.paellasoft@gmail.com>
2026-04-15 10:06:51 +0200

    Añadida interfaz usuarios
    Para, en un futuro, poder añadir imágenes de perfil a los usuarios.

---
+++ src/app/services/api-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { User, UsersInterfaces } from '../common/interfaces/users-interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private readonly http: HttpClient = inject(HttpClient);
+  private readonly baseUrl = 'https://dummyjson.com/users';
+
+
+  getUsersMOCK(): Observable<User[]> {
+    const campos = 'firstName,lastName,age,username,image';
+    return this.http.get<UsersInterfaces>(`${this.baseUrl}?limit=30&select=${campos}`)
+      .pipe(map((respuesta: UsersInterfaces) => respuesta.users));
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/${id}`);
+  }
+
+  getUserByFilter(filtro: string[], limit: number): Observable<User[]> {
+    return this.http.get<UsersInterfaces>(`${this.baseUrl}?limit=${limit}&select=${filtro.join(',')}`)
+      .pipe(map((respuesta: UsersInterfaces) => respuesta.users));
+  }
+
+}
