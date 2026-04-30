c145339d91a7c5f6a06c52a04176e1e9799542d3
Agus MT <amontoya.paellasoft@gmail.com>
2026-04-30 13:07:29 +0200

    Creado menú lateral
    y editada consola general

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { AgentMap } from './components/agent-map/agent-map';
 import { TodoComponent } from './components/to-do/to-do';
+import { Configuracion } from './components/configuracion/configuracion';
 
 export const routes: Routes = [
   { path: '', component: AgentMap },
   { path: 'to-do', component: TodoComponent },
   { path: 'to-do/:userId/:userName', component: TodoComponent },
   { path: 'to-do/:userId', component: TodoComponent },
-  { path: 'to-do/:id/:agentId', component: TodoComponent }
+  { path: 'to-do/:id/:agentId', component: TodoComponent },
+  { path: 'configuracion', component: Configuracion },
 ];
