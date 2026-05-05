b21417a1c95ac5f99ee2dc95c20a54898dbb0a41
Agus MT <amontoya.paellasoft@gmail.com>
2026-05-05 10:03:47 +0200

    Modificados es.json y en.json
    Estructura nueva que respeta las secciones

--- src/app/components/consola-sistema/consola-sistema.ts
+++ src/app/components/consola-sistema/consola-sistema.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { Chat } from '../chat/chat';
 import { WorkspaceService } from '../../services/workspace-service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-consola-sistema',
   standalone: true,
-  imports: [Chat],
+  imports: [Chat, TranslateModule],
   templateUrl: './consola-sistema.html',
   styleUrl: './consola-sistema.css',
 })
